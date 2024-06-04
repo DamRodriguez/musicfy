486e9a7cf51f755975d47276600f8c5e6f715061
Damian Rodriguez <141194257+DamRodriguez@users.noreply.github.com>
2024-06-04 20:28:36 -0300

    Efectos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+/*reproduccion de canciones*/
+
 const audios = document.querySelectorAll(".audio");
 const playButtons = document.querySelectorAll(".play");
+
+let currentAudio = null;
 
 playButtons.forEach((playButton, index) => {
     const audio = audios[index];
@@ -7,19 +11,33 @@ playButtons.forEach((playButton, index) => {
     const botonPause = playButton.querySelector(".boton-pausa");
 
     playButton.addEventListener("click", () => {
+
+        if (currentAudio && currentAudio !== audio) {
+            currentAudio.pause();
+            const currentButton = playButtons[Array.from(audios).indexOf(currentAudio)];
+            const currentPlayButton = currentButton.querySelector(".boton-play");
+            const currentPauseButton = currentButton.querySelector(".boton-pausa");
+            currentPauseButton.classList.add("hide");
+            currentPlayButton.classList.remove("hide");
+        }
+
         if (audio.paused) {
             botonPause.classList.remove("hide");
             botonPlay.classList.add("hide");
             audio.play();
+            currentAudio = audio; 
         } else {
             audio.pause();
             botonPause.classList.add("hide");
             botonPlay.classList.remove("hide");
+            currentAudio = null; 
         }
     });
 });
 
-/*----------------------validacion registro------------------------------*/
+
+
+/*validacion registro*/
 
 function validar(Document){
     if(Document ==="Login"){
@@ -227,3 +245,106 @@ function validar(Document){
         formRegistro.addEventListener('submit', validarDatos)
     }
 }
+
+/* hover index */
+
+var item1 = document.querySelector(".lista-index1 .item-depeche");
+var item2 = document.querySelector(".lista-index1 .item-circle");
+var item3 = document.querySelector(".lista-index1 .item-cure");
+var item4 = document.querySelector(".lista-index1 .item-one");
+var item5 = document.querySelector(".lista-index1 .item-boris");
+var item6 = document.querySelector(".lista-index1 .item-nafta");
+var fondo = document.querySelector(".fondo2-index");
+
+/*depeche mode*/
+
+item1.addEventListener("mouseover", function() {
+
+fondo.classList.add("fondo-depeche");
+fondo.classList.remove("fondo2-index");
+
+});
+
+item1.addEventListener("mouseout", function() {
+fondo.classList.add("fondo2-index");
+fondo.classList.remove("fondo-depeche");
+
+});
+
+/*perfect circle*/
+
+item2.addEventListener("mouseover", function() {
+
+fondo.classList.add("fondo-circle");
+fondo.classList.remove("fondo2-index");
+    
+});
+    
+item2.addEventListener("mouseout", function() {
+fondo.classList.add("fondo2-index");
+fondo.classList.remove("fondo-circle");
+
+});
+
+/*the cure*/
+
+item3.addEventListener("mouseover", function() {
+
+fondo.classList.add("fondo-cure");
+fondo.classList.remove("fondo2-index");
+    
+});
+    
+item3.addEventListener("mouseout", function() {
+fondo.classList.add("fondo2-index");
+fondo.classList.remove("fondo-cure");
+
+});
+
+/*and one*/
+
+item4.addEventListener("mouseover", function() {
+
+fondo.classList.add("fondo-one");
+fondo.classList.remove("fondo2-index");
+    
+});
+    
+item4.addEventListener("mouseout", function() {
+fondo.classList.add("fondo2-index");
+fondo.classList.remove("fondo-one");
+
+});
+
+/*boris brejcha*/
+
+item5.addEventListener("mouseover", function() {
+
+fondo.classList.add("fondo-boris");
+fondo.classList.remove("fondo2-index");
+    
+});
+    
+item5.addEventListener("mouseout", function() {
+fondo.classList.add("fondo2-index");
+fondo.classList.remove("fondo-boris");
+
+});
+
+/*nafta*/
+
+item6.addEventListener("mouseover", function() {
+
+fondo.classList.add("fondo-nafta");
+fondo.classList.remove("fondo2-index");
+    
+});
+    
+item6.addEventListener("mouseout", function() {
+fondo.classList.add("fondo2-index");
+fondo.classList.remove("fondo-nafta");
+
+});
+
+
+
